bbcc29e7c5bce06736be2e47e7f108a9a38ef1a9
A <abbasgure@gmail.com>
2024-10-10 05:51:44 +0100

    added versioning functionality as well as documentation for program.cs

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -7,82 +7,57 @@ using Serilog;
 using AutoMapper;
 using Microsoft.IdentityModel.Tokens;
 using System.Reflection;
+using Asp.Versioning.ApiExplorer;
 
-//serilog logger created 
+// Serilog logger configuration for logging
 Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Debug()
-    .WriteTo.Console()
-    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .MinimumLevel.Debug() // Set the minimum log level
+    .WriteTo.Console() // Log to console
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day) // Log to a file with daily rolling
     .CreateLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-//commented out due to using serilog instead
-//builder.Logging.ClearProviders();
-//builder.Logging.AddConsole();
+var builder = WebApplication.CreateBuilder(args); // Create a new web application builder
+
+// Use Serilog for logging
 builder.Host.UseSerilog();
 
-// add logger
-
-
 // Add services to the container.
-
 builder.Services.AddControllers(options =>
 {
+    options.ReturnHttpNotAcceptable = true; // Prevent sending data if the requested format doesn't match
+})
+.AddNewtonsoftJson() // Replace default JSON formatters with Newtonsoft.Json
+.AddXmlDataContractSerializerFormatters(); // Support XML responses
 
-    //can do options.inputformatter/outputformatter and the first formatter for each will be set at the default one 
-
-    options.ReturnHttpNotAcceptable = true;//To prevent sending data if our format doesnt match the format the client requests
-}).AddNewtonsoftJson() //replaces default json formatters with json.net
-    .AddXmlDataContractSerializerFormatters(); //Added xml format to our responses by calling this one method
-
-
-
-//Testing Problem details
+// Add Problem Details service for standardized error responses
 builder.Services.AddProblemDetails();
 
-
-
-//builder.Services.AddProblemDetails(options =>
-//{
-//    options.CustomizeProblemDetails = ctx =>
-//    {
-//        ctx.ProblemDetails.Extensions.Add("additionalInfo", "Testing additional info with bad request");
-//        ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
-//    };
-//});
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add the API Explorer for Swagger documentation
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(setupAction =>
-{
-    var cityApi = $"{Assembly.GetExecutingAssembly().GetName().Name}";
-    var xmlCommentsFile = $"{cityApi}.xml";
-    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
 
-    setupAction.IncludeXmlComments(xmlCommentsFullPath);
-});
-builder.Services.AddSingleton<FileExtensionContentTypeProvider>(); // allows us to inject fileextensiontypecontent provider to provide support for files with different content types e.g. application/pdf
+// File extension content type provider for serving files with specific content types
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
+// Conditional service registration based on build configuration
 #if DEBUG
-//adding local mail service (interface, implementation class), IoC Principle, trying to decouple implementations from services 
-builder.Services.AddTransient<IMailService,LocalMailService>();
-#elif RELEASE //we use the cloud mail service
-builder.Services.AddTransient<IMailService,CloudMailService>();
+// Local mail service for development
+builder.Services.AddTransient<IMailService, LocalMailService>();
+#elif RELEASE 
+// Cloud mail service for production
+builder.Services.AddTransient<IMailService, CloudMailService>();
 #endif
-//adding datastore as a singleton
-//builder.Services.AddSingleton<CityInfo.API.ICityInfoRepository>();
 
-//adding my DbContext
-builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
+// Register the DbContext with SQLite configuration
+builder.Services.AddDbContext<CityInfoContext>(dbContextOptions =>
+    dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
-//Creating scoped datastore
+// Scoped service for repository pattern
 builder.Services.AddScoped<CityInfo.API.Services.ICityInfoRepository, CityInfoRepository>();
 
-//using Automapper to map from city object variables with dto
+// Add AutoMapper for object mapping
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-//jwt token bearer
+// JWT authentication configuration
 builder.Services.AddAuthentication("Bearer").AddJwtBearer(options =>
 {
     options.TokenValidationParameters = new()
@@ -93,63 +68,86 @@ builder.Services.AddAuthentication("Bearer").AddJwtBearer(options =>
         ValidIssuer = builder.Configuration["Authentication:Issuer"],
         ValidAudience = builder.Configuration["Authentication:Audience"],
         IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(builder.Configuration["Authentication:SecretForeignKey"]))
-
     };
 });
 
-/* There are different Access control systems such as [MAC] Mandatory access control, Policy based access control [PBAC] 
- * Lets try dotnets PBAC 
- * Adding policy
- */
-
+// Adding Authorization policies
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("MustBeFromSpij", policy =>
     {
         policy.RequireAuthenticatedUser();
-        policy.RequireClaim("city", "Xamar City");
+        policy.RequireClaim("city", "Xamar City"); // Custom claim requirement
     });
 });
 
-
-/**
- * Setting up versioning
- */
-
+// Setting up API versioning
 builder.Services.AddApiVersioning(setupAction =>
 {
-    setupAction.ReportApiVersions = true;
-    setupAction.AssumeDefaultVersionWhenUnspecified = true;
-    setupAction.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
-}).AddMvc();
+    setupAction.ReportApiVersions = true; // Enable version reporting
+    setupAction.AssumeDefaultVersionWhenUnspecified = true; // Assume default version
+    setupAction.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0); // Set default version
+}).AddMvc()
+.AddApiExplorer(setupAction =>
+{
+    setupAction.SubstituteApiVersionInUrl = true; // Enable version substitution in URL
+});
 
+// Swagger documentation setup
+var apiVersionDescriptionProvider = builder.Services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
 
-var app = builder.Build();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+    {
+        setupAction.SwaggerDoc(
+            $"{description.GroupName}",
+            new()
+            {
+                Title = "City Info API", // Title of the API
+                Version = description.ApiVersion.ToString(), // API version
+                Description = "Access cities and see their points of interest through this API." // API description
+            });
+    }
 
-// Configure the HTTP request pipeline.
-//check for if the application is not in development mode
-if (!app.Environment.IsDevelopment())app.UseExceptionHandler();
+    // Include XML comments for Swagger documentation
+    var cityApi = $"{Assembly.GetExecutingAssembly().GetName().Name}";
+    var xmlCommentsFile = $"{cityApi}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+});
+
+var app = builder.Build(); // Build the application
+
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+    app.UseExceptionHandler(); // Use exception handler in non-development environments
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(); // Enable Swagger in development
+    app.UseSwaggerUI(setupAction =>
+    {
+        var descriptions = app.DescribeApiVersions();
+        foreach (var desc in descriptions)
+        {
+            setupAction.SwaggerEndpoint(
+                $"/swagger/{desc.GroupName}/swagger.json", // Endpoint for the Swagger UI
+                desc.GroupName.ToUpperInvariant()); // Group name in uppercase
+        }
+    });
 }
 
-
-//use middlewares
+// Use middleware for HTTPS redirection, routing, authentication, and authorization
 app.UseHttpsRedirection();
-
 app.UseRouting();
+app.UseAuthentication(); // Enable authentication
+app.UseAuthorization(); // Enable authorization
 
-app.UseAuthentication(); //use authentication
-
-app.UseAuthorization();
-
+// Map controllers to endpoints
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
 
-
-app.Run();
+app.Run(); // Run the application
