dda93d03b2b4e0cbd240a3318c90218d7658eab6
A <abbasgure@gmail.com>
2024-10-10 04:30:51 +0100

    Providing documentation for entities

--- CityInfo.API/Entities/City.cs
+++ CityInfo.API/Entities/City.cs
@@ -4,26 +4,49 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CityInfo.API.Entities
 {
+    /// <summary>
+    ///  Entity class representing City Object in a Database, is tightly coupled with the Database schema, is required for object to be saved to and retrieved from the Database 
+    /// </summary>
     public class City
     {
+
         /**
-         * You will notice in our DTO we dont add requirements
-         * And that is because DTOs are only used to get data, not manipulate it to no need for regulations
+         * You will notice in our DTO we dont add requirements but you will find requirements here in this class
+         * And that is because DTOs are only used to get data, not manipulate it so hence no need for regulations
          */
+
+        /// <summary>
+        /// Id of City (id column in db)
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        
         public int Id { get; set; }
 
+        /// <summary>
+        /// Name of city (name column in db)
+        /// </summary>
         [Required]
         [MaxLength(50)] 
         public string Name { get; set; }
 
+
+        /// <summary>
+        /// Description of City (description column in db), optional
+        /// </summary>
         [MaxLength(200)]
         public string?   Description { get; set; }
 
+        /// <summary>
+        /// Points of Interests in the form of List Collection
+        /// </summary>
         public ICollection<PointOfInterest> PointsOfInterest { get; set; }
         = new List<PointOfInterest>();
 
+        /// <summary>
+        /// Constructor of City class
+        /// </summary>
+        /// <param name="name">Name of City</param>
         public City(string name) {  Name = name; }
     }
 }
--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -3,25 +3,48 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CityInfo.API.Entities
 {
+    /// <summary>
+    ///  Entity class representing Point of Interest Object in a Database, is tightly coupled with the Database schema, is required for object to be saved to and retrieved from the Database 
+    /// </summary>
     public class PointOfInterest
     {
-
+        /// <summary>
+        /// Id of Point of Interest
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        /// <summary>
+        /// Name of Point of Interest
+        /// </summary>
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
 
+
+        /// <summary>
+        /// Description of Point of Interest
+        /// </summary>
         [MaxLength(200)]
         public string? Description { get; set; }
 
-        //since pointofinterest is part of city, adding city property sets up a relation between the two that some dbs can map and use for navigation, we set cityid as the foreign key that it is
+        /// <summary>
+        /// since pointofinterest is part of city, adding city property sets up a relation between the two that some dbs can map and use for navigation
+        /// we set cityid as the foreign key
+        /// </summary>
+
         [ForeignKey("CityId")]
         public City? City { get; set; }
 
+        /// <summary>
+        /// City id FK
+        /// </summary>
         public int CityId { get; set; }
 
+        /// <summary>
+        /// Constructor of Point of Interest
+        /// </summary>
+        /// <param name="name">Name of Point of Interest</param>
         public PointOfInterest(string name) { Name = name; }
     }
 }
